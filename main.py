16bd869404cd5488b05e242ef0e1b83116273014
PandEwok <diegojaillot@outlook.fr>
2023-12-13 19:48:17 +0100

    hitbox (modification du déplacement)

--- main.py
+++ main.py
@@ -37,22 +37,94 @@ class Soldier(pygame.sprite.Sprite):
 
         # Assign movement variable if moving
         if moving_left:
-            dx = -self.speed
+            for entity in entity_list:
+                if (not entity==self) and (self.hitbox.collide(entity)):
+                    dx = self.speed*2.1
+                    break
+            dx += -self.speed
         if moving_right:
-            dx = self.speed
+            for entity in entity_list:
+                if (not entity==self) and (self.hitbox.collide(entity)):
+                    dx = -self.speed*2.1
+                    break
+            dx += self.speed
         if moving_up:
-            dy = -self.speed
+            for entity in entity_list:
+                if (not entity==self) and (self.hitbox.collide(entity)):
+                    dy = self.speed*2.1
+                    break
+            dy += -self.speed
         if moving_down:
-            dy = self.speed
+            for entity in entity_list:
+                if (not entity==self) and (self.hitbox.collide(entity)):
+                    dy = -self.speed*2.1
+                    break
+            dy += self.speed
+
+        # Update Rectangle position
+        self.rect.x += dx
+        self.rect.y += dy
+
+        self.hitbox = Hitbox(self.rect.x, self.rect.y, self.image)
+
+    # Drawing the Soldier to the screen
+    def draw(self):
+        screen.blit(self.image, self.rect)
+
+
+# Same as Soldier class for now, for testing purpose
+class Entity(pygame.sprite.Sprite):
+    def __init__(self, x= 400, y=100, scale=0.1, speed=3):
+        pygame.sprite.Sprite.__init__(self)
+        self.speed = speed
+        img = pygame.image.load('img/player/idle/0.png')
+        self.image = pygame.transform.scale(img, (int(img.get_width() * scale), int(img.get_height() * scale)))
+        self.rect = self.image.get_rect()
+        self.rect.center = (x, y)
+        self.hitbox = Hitbox(x, y, self.image)
+
+    def move(self, moving_left, moving_right, moving_up, moving_down):
+        # reset movement variables
+        dx = 0
+        dy = 0
+
+
+        # Assign movement variable if moving
+        if moving_left:
+            for entity in entity_list:
+                if (not entity==self) and (self.hitbox.collide(entity)):
+                    dx = self.speed*2.1
+                    break
+            dx += -self.speed
+        if moving_right:
+            for entity in entity_list:
+                if (not entity==self) and (self.hitbox.collide(entity)):
+                    dx = -self.speed*2.1
+                    break
+            dx += self.speed
+        if moving_up:
+            for entity in entity_list:
+                if (not entity==self) and (self.hitbox.collide(entity)):
+                    dy = self.speed*2.1
+                    break
+            dy += -self.speed
+        if moving_down:
+            for entity in entity_list:
+                if (not entity==self) and (self.hitbox.collide(entity)):
+                    dy = -self.speed*2.1
+                    break
+            dy += self.speed
 
         # Update Rectange position
         self.rect.x += dx
         self.rect.y += dy
 
-    # Drawing the Soldier to the screen
+        self.hitbox = Hitbox(self.rect.x, self.rect.y, self.image)
+
+    # Drawing the Entity to the screen
     def draw(self):
         screen.blit(self.image, self.rect)
-        
+
 
 # Defining the Map class
 class Map(pygame.sprite.Sprite):
@@ -63,7 +135,7 @@ class Map(pygame.sprite.Sprite):
         self.image = pygame.transform.scale(img, (int(img.get_width() * scale), int(img.get_height() * scale)))
         self.rect = self.image.get_rect()
         self.rect.center = (x, y)
-    
+
     # Drawing map
     def draw(self):
         screen.blit(self.image, self.rect)
@@ -71,17 +143,21 @@ class Map(pygame.sprite.Sprite):
 
 # Defining the Hitbox class ; x is x position of the centre of the hitbox, same for y, and W is the width and H the height
 class Hitbox:
-    def __init__(self, x, y, W, H):
-        self.left, self.right = x - W/2, x + W/2
-        self.up, self.down = y - H/2, y + H/2
-        
+    def __init__(self, x, y, img):
+        self.rect = img.get_rect()
+        self.rect.center = (x+self.rect.width/2, y+self.rect.height/2)
+
     def collide(self, other):
-        assert isinstance(other, Hitbox), "Can't collide Hitbox with another type"
-        return self.left==other.right or self.right==other.left or self.up==other.down
-        
+        # assert isinstance(other, Hitbox) or isinstance(other, Soldier), f"Can't collide Hitbox with another type than Hitbox or Soldier : attempted to collide {other}"
+        return self.rect.colliderect(other.rect)
+
 
 # Creating the Player
 player = Soldier()
+
+monster = Entity()
+
+entity_list = [player, monster]
 
 map1 = Map()
 
@@ -100,6 +176,8 @@ while run:
 
     # Drawing the player on the screen
     player.draw()
+    
+    monster.draw()
     
     # Moving the player
     player.move(moving_left, moving_right, moving_up, moving_down)
